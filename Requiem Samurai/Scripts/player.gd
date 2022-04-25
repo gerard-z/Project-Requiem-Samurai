a53b8e7550b1895146695e5f6e27081e9d0abbd2
Matías Mancilla Lizana <m.mancillalizana@gmail.com>
2022-04-25 12:50:27 -0400

    prueba trazado b)
    Para dejar puntos: R1 o click derecho
    Para activar figura: Y (mando) o E

--- Requiem Samurai/Scripts/player.gd
+++ Requiem Samurai/Scripts/player.gd
@@ -35,17 +35,19 @@ func _physics_process(delta): # por frame
 	velocity.x = move_input * SPEED
 
 	up_down = 1
-
+	
+	# gravedad
+	velocity.y += GRAVITY * delta * DIR
 	
 # PISO
-	if is_on_floor():
+	if is_on_floor() and not is_on_wall():
 		# salto
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -4 * SPEED
 		
 	
 # MURO
-	elif is_on_wall():
+	elif is_on_wall() and not is_on_floor():
 
 		#	#		#velocity.y = GRAVITY * 0.25
 
@@ -55,20 +57,12 @@ func _physics_process(delta): # por frame
 
 
 		elif Input.is_action_pressed("move_down") and not Input.is_action_just_pressed("move_up"):
-			up_down = 1
+			up_down = 300
 		elif velocity.y >0:
-			up_down = 0.25
+			up_down = 25
+		
+		velocity.y = up_down
 
-
-
-		########## FALTA ##########
-
-		# wall jumps
-		if Input.is_action_pressed("move_right") and not Input.is_action_just_pressed("move_left"):
-			pass
-
-		if Input.is_action_pressed("move_left") and not Input.is_action_just_pressed("move_right"):
-			pass
 
 
 		# wall dash
@@ -82,11 +76,10 @@ func _physics_process(delta): # por frame
 			velocity.x = velocity.x + fwall
 			velocity.y = -4 * SPEED
 			
-		
-	# gravedad
-	velocity.y += GRAVITY * delta * DIR * up_down
+
 # TODO
-	var dash = 10
+	var dash = 20000
+	var dirdash = 1
 	# voltear player al cambiar de dirección
 	if Input.is_action_pressed("move_right") and not Input.is_action_just_pressed("move_left"):
 		pivote.scale.x = 1
@@ -95,21 +88,23 @@ func _physics_process(delta): # por frame
 		pivote.scale.x = -1
 	
 	hasAttacked = false
-	# ataque 1
+	# ataque 1 + dash
 	if Input.is_action_just_pressed("attack1") and not is_on_wall():
-		if pivote.scale.x<0:
-			dash = -40
+		if pivote.scale.x < 0:
+			dirdash = -abs(dirdash)
 		else:
-			dash = 40
-		
+			dirdash = abs(dirdash)
 		hasAttacked = true
-		velocity.x = velocity.x +  dash * SPEED
+		
 	
 # ANIMACIONES	
 
 # Animaciones
 	if hasAttacked:
 		playback.travel("attack 1")
+		velocity.x =  dash * dirdash #dash
+
+		
 	else:
 		if is_on_floor():
 			if abs(velocity.x) > 1:
@@ -119,7 +114,7 @@ func _physics_process(delta): # por frame
 		
 		elif is_on_wall():
 			playback.travel("idle wall")
-			if velocity.y > 0:
+			if velocity.y > 100:
 				playback.travel("fall wall")
 		else:
 		# animacion caida y salto
--- Requiem Samurai/Scripts/traced.gd
+++ Requiem Samurai/Scripts/traced.gd
@@ -1,30 +1,51 @@
 extends Line2D
 
-onready var timer = $Timer
-onready var sennal = $Polygon2D
-var target
-export(NodePath) var targetPath
-export var trailLenght = 0
-var cercano
+onready var sennal = $Polygon2D			# relleno
+var target								# trazado samurai
+export(NodePath) var targetPath			# dirección samurai
+export var trailLenght = 0				# largo trazado, n° vértices
+#var cercano								# medir dist.
 var dibujable = true
+var point
 
 func _ready():
-	target = get_node(targetPath)
+	target = get_node(targetPath)		# carga nodo samurai, posicion
 
 func _process(delta):
-	global_position = Vector2(0,0)
+	global_position = Vector2(0,0)		
 	global_rotation = 0
-	add_point(target.global_position)
-	if get_point_count() > trailLenght:
-		remove_point(0)
-	cercano = get_point_position(trailLenght-1).distance_to(get_point_position(0))
-	if cercano < 50 and dibujable:
+	
+	if Input.is_action_just_pressed("vertice"):
+		point = target.global_position
+		point.y = point.y - 48
+		
+		add_point(point)	# se agrega punto actual en lista ***
+		
+	if get_point_count() > trailLenght-1:	# largo lista > largo def
+		remove_point(0)					# se borra de lista el primero
+	
+	#cercano = get_point_position(trailLenght-1).distance_to(get_point_position(0)) #dist entre ult y primer punto
+	
+	if Input.is_action_just_pressed("activar"):	
+		add_point(get_point_position(0))
 		print("Cierre")
 		sennal.visible = true
 		sennal.set_polygon(get_points())
 		dibujable = false
-		yield(get_tree().create_timer(3), "timeout")
+		
+		yield(get_tree().create_timer(1), "timeout")
+		
+		clear_points()
 		sennal.visible = false
 		dibujable = true
+	
+	#if cercano < 50 and dibujable:
+	#	print("Cierre")
+	#	sennal.visible = true 			# se puede dibujar polígono
+	#	sennal.set_polygon(get_points())# guarda vértices de trazado
+	#	dibujable = false				# se deja de dibujar, solo dibuja 1
+	#	yield(get_tree().create_timer(3), "timeout")	# tiempo, espera 3 segundos para continuar
+	#	sennal.visible = false
+	#	dibujable = true
 		
 	
