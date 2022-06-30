e4dd2a8b16beeb76b5cf4f5145918ae6b0b8d38e
Matías Mancilla Lizana <m.mancillalizana@gmail.com>
2022-06-30 04:48:53 -0400

    Escenario Inicio Parte 1

--- Requiem Samurai/Scripts/MainMenu.gd
+++ Requiem Samurai/Scripts/MainMenu.gd
@@ -10,7 +10,8 @@ func _ready():
 	exit.connect("pressed", self, "_on_exit_pressed")
 	
 func _on_play_pressed():
-	get_tree().change_scene("res://Escenes/main.tscn")
+	get_tree().change_scene("res://Escenes/Intro.tscn")
+	#get_tree().change_scene("res://Escenes/main.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
