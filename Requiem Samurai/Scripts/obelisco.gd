e14f4613d49451adcbb3f058eb092e9f113151f2
Matías Mancilla Lizana <m.mancillalizana@gmail.com>
2022-07-01 04:21:49 -0400

    Actualización Level 0
    +estética
    +decoración
    +fondo con movimiento según posición del jugador
    +tilemap

---
+++ Requiem Samurai/Scripts/obelisco.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+onready var anim_player = $AnimationPlayer
+onready var anim_tree = $AnimationTree
+onready var playback = anim_tree.get("parameters/playback")	
+
+func _ready():
+	anim_tree.active = true
+	playback.start("idle")
