ec9dc12b5940f6ca1767e71c3c86a1ef3f85a406
FelipeFbh <felipe.olivares.e@ing.uchile.cl>
2022-07-21 18:31:15 -0400

    animaciones espadas

--- Requiem Samurai/Scripts/Global.gd
+++ Requiem Samurai/Scripts/Global.gd
@@ -23,7 +23,7 @@ var lvlskill = 4 #habilidades desbloqueadas
 var elementalsword = 0
 var ataqpyro = 0 #1
 var ataqhydro= 0 #2
-var ataqwind = 0 #3
+var ataqearth = 0 #3
 
 var gravitychange = 1
 
@@ -51,10 +51,10 @@ func _process(delta): #tiempo => frames
 			ataqhydro=0
 			ataqpyro=0
 			
-			ataqwind+=3
-			if ataqwind>=4:
-				ataqwind=4
-			print("attackwind = ", ataqwind)
+			ataqearth+=3
+			if ataqearth>=4:
+				ataqearth=4
+			print("attack earth = ", ataqearth)
 	
 	if area != 0:
 		print(figure, " ", area)
@@ -64,7 +64,7 @@ func _process(delta): #tiempo => frames
 			if type_figure==0: #Pyro Sword
 				elementalsword=1
 				ataqhydro=0
-				ataqwind=0
+				ataqearth=0
 				
 				ataqpyro+=3
 				if ataqpyro>=4:
@@ -74,7 +74,7 @@ func _process(delta): #tiempo => frames
 			else: #Hydro_sword
 				elementalsword=2
 				ataqpyro=0
-				ataqwind=0
+				ataqearth=0
 				ataqhydro =3
 				if ataqhydro>=4:
 					ataqhydro=4
@@ -108,6 +108,10 @@ func _gravity_time_power():
 	
 	if time_elapsed >= 150:
 		gravitychange=1
+
+
+
+
 
 
 #utilizar fps para el tiempo #nota para mi yo del futuro planeando
